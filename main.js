e2007222752c7eff75bf5a06025224dd0ef07c74
Mara Hernandez <mara.hesp@gmail.com>
2022-09-30 17:36:38 -0500

    second-commit

--- main.js
+++ main.js
@@ -2,26 +2,29 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
         .then(res => res.json())
         .then(data => {
     document.getElementById('title').innerText = data.name
+
     
     const dataset = data.data
+    let dollarLocale = Intl.NumberFormat('en-US')
 
-    const minX = d3.min(dataset, d => d[0]);
-    const maxX = d3.max(dataset, d => d[0]);
+    const minX = d3.min(dataset, d => parseInt(d[0]));
+    const maxX = d3.max(dataset, d => parseInt(d[0]));
 
-    const minY = d3.min(dataset, d => d[1]);
     const maxY = d3.max(dataset, d => d[1])
 
     const w = 1200
     const h = 500
-    const padding = 25
+    const padding = 50
 
     const xScale = d3.scaleLinear()
-                    .domain([0, maxX])
+                    .domain([minX, maxX])
                     .range([padding, w - padding])
+
     const yScale = d3.scaleLinear()
                     .domain([0, maxY])
                     .range([h - padding, padding])
 
+                    
     const svg = d3.select('div')
                     .append('svg')
                     .attr('width', w)
@@ -31,10 +34,24 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
        .data(dataset)
        .enter()
        .append("rect")
-       .attr("x", (d, i) => i * 4)
-       .attr("y", (d, i) => h - d[1])
+       .attr("x", (d, i) => (i + 13) * 4)
+       .attr('y', (d, i) => yScale(d[1]))
        .attr("width", 3)
-       .attr("height", (d => d[1]))
+       .attr("height", d => h - padding - yScale(d[1]))
        .attr("class", "bar")
-    
+       .append('title')
+       .text(d => `${d[0]}\n$${dollarLocale.format(d[1])} billion`)
+
+       let xAxis = d3.axisBottom(xScale)
+       let yAxis = d3.axisLeft(yScale)
+
+        svg.append('g')
+        .attr('transform', `translate(0, ${h - padding})`)
+        .call(xAxis)
+
+        svg.append('g')
+        .attr('transform', `translate(${padding}, 0)`)
+        .call(yAxis)
+
+
 })
